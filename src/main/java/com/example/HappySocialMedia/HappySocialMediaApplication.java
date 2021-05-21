49495ef23a9593f6b837346becdef47d5e5a2e90
jack160296 <s3811248@rmit.edu.vn>
2021-05-21 10:58:30 +0700

    design structure

---
+++ src/main/java/com/example/HappySocialMedia/HappySocialMediaApplication.java
@@ -0,0 +1,16 @@
+package com.example.HappySocialMedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class HappySocialMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappySocialMediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HappySocialMedia/HappySocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HappySocialMedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappySocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
