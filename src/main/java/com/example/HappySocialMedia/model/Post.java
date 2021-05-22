f361430596cb45215bed869c62e6f206813be6ac
Nhat180 <s3740975@rmit.edu.vn>
2021-05-22 11:02:19 +0700

    Create Post.java

---
+++ src/main/java/com/example/HappySocialMedia/model/Post.java
@@ -0,0 +1,34 @@
+package com.example.HappyTweet.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
+
+import javax.validation.constraints.NotBlank;
+import java.time.Instant;
+
+import javax.persistence.*;
+import static javax.persistence.GenerationType.IDENTITY;
+import static javax.persistence.FetchType.LAZY;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long postId;
+    @NotBlank(message = "Post Name must not be empty or null")
+    private String postName;
+    @Nullable
+    @Lob
+    private String description;
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    @ManyToOne(fetch = LAZY)
+    private User user;
+    private Instant createdDate;
+}
