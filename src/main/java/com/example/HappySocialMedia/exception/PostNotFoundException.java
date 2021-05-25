cb8b0c2c77c71e626e81753c65aa4c9868457259
TranThienVan <s3740845@rmit.edu.vn>
2021-05-25 14:22:58 +0700

    Adding comments to the exception section

--- src/main/java/com/example/HappySocialMedia/exception/PostNotFoundException.java
+++ src/main/java/com/example/HappySocialMedia/exception/PostNotFoundException.java
@@ -1,5 +1,6 @@
 package com.example.HappySocialMedia.exception;
 
+// This class will handle the founding post section, if the post is not found, the website will display a message error
 public class PostNotFoundException extends RuntimeException {
     public PostNotFoundException(String exceptionMessage) {
         super(exceptionMessage);
--- src/main/java/com/example/HappySocialMedia/exception/ServerException.java
+++ src/main/java/com/example/HappySocialMedia/exception/ServerException.java
@@ -1,5 +1,6 @@
 package com.example.HappySocialMedia.exception;
 
+// This class will handle the serverException section, if the website meet errors when loading or retrieving key store, the website will display an error message
 public class ServerException extends RuntimeException {
     public ServerException(String exceptionMessage, Exception exception) {
         super(exceptionMessage, exception);
